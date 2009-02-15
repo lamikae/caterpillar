6d5b0c425f3065bc446258d380dd9ed8f39c7a3a
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-02-15 10:07:34 +0000

    added some tables and a notice to sequence migrations

--- db/migrate/20081205000002_lportal_sequences.rb
+++ db/migrate/20081205000002_lportal_sequences.rb
@@ -14,10 +14,15 @@ class LportalSequences < ActiveRecord::Migration
     Resource,
     Role,
     User,
-    MB::Message,
-    MB::Thread,
-    MB::Discussion,
     MB::Category,
+    MB::Discussion,
+    MB::Message,
+    MB::MessageFlag,
+    MB::StatsUser,
+    MB::Thread,
+    RatingsStats,
+    SocialActivity,
+    SocialRelation,
     Tag::Asset,
     Tag::Entry,
     Tag::Property,
@@ -28,25 +33,33 @@ class LportalSequences < ActiveRecord::Migration
   ]
 
   def self.up
-#     start = 8400000 # bigint = 8^8 bytes = 16 million bits, this is halfway up the possible range, rounded up
-#     sql = ""
-#     @@tables.each do |model|
-#       table = model.table_name
-#       primkey = model.primary_key
-#       seq = table+'_'+primkey+'_seq'
-#       sql += "CREATE SEQUENCE #{seq} START #{start}; ALTER TABLE #{table} ALTER #{primkey} SET default nextval('#{seq}');"
-#     end
-#     ActiveRecord::Base.connection.execute(sql)
+    STDOUT.puts 'This migration does not do anything.'
+    STDOUT.puts 'The process is not refined properly yet, and could be quite disastrous if reverted unappropriately.'
+    STDOUT.puts 'If you are sure you need the sequences, copy this file to db/migrate and modify it.'
+    STDOUT.puts __FILE__
+
+    start = 8400000 # bigint = 8^8 bytes = 16 million bits, this is halfway up the possible range, rounded up
+    sql = ""
+    @@tables.each do |model|
+      table = model.table_name
+      primkey = model.primary_key
+      seq = table+'_'+primkey+'_seq'
+      sql += "CREATE SEQUENCE #{seq} START #{start}; ALTER TABLE #{table} ALTER #{primkey} SET default nextval('#{seq}');"
+    end
+
+    # To activate, uncomment this line.
+    #ActiveRecord::Base.connection.execute(sql)
   end
 
+  # This is VERY DANGEROUS and may lead to breakage.
   def self.down
-#     sql = ""
-#     @@tables.each do |model|
-#       table = model.table_name
-#       primkey = model.primary_key
-#       seq = table+'_'+primkey+'_seq'
-#       sql += "ALTER TABLE #{table} ALTER #{primkey} DROP default; DROP SEQUENCE #{seq};"
-#     end
-#     ActiveRecord::Base.connection.execute(sql)
+    # sql = ""
+    # @@tables.each do |model|
+    #   table = model.table_name
+    #   primkey = model.primary_key
+    #   seq = table+'_'+primkey+'_seq'
+    #   sql += "ALTER TABLE #{table} ALTER #{primkey} DROP default; DROP SEQUENCE #{seq};"
+    # end
+    # ActiveRecord::Base.connection.execute(sql)
   end
 end
