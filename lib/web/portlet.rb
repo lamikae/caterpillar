bc95625ad6a3f70353a2e77c3de1ba4c2975c164
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-02-23 16:02:49 +0000

    model Rails portlets as Web::Portlets

--- lib/web/portlet.rb
+++ lib/web/portlet.rb
@@ -2,9 +2,30 @@ require 'active_record'
 
 module Web
   class Portlet < ActiveRecord::Base
+
+    @@caterpillar_portlets = nil
+
+    def self.caterpillar_portlets
+      return @@caterpillar_portlets if @@caterpillar_portlets
+
+      config = Caterpillar::Util.eval_configuration
+      config.routes = Caterpillar::Util.parse_routes(config)
+
+      # transform objects
+      portlets = []
+      Caterpillar::Parser.new(config).portlets.each do |p|
+        portlets << self.new(
+          :portletid => p[:name].to_s
+        )
+      end
+
+      @@caterpillar_portlets = portlets
+    end
+
     def title
       p = Web::PortletName.find_by_portletid(self.portletid)
       p ? p.title : nil
     end
+
   end
 end
