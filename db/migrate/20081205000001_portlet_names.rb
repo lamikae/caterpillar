8e214b6780cd27989f6ebe12185246d12a06ba52
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-02-25 09:16:11 +0000

    add column instanceable

--- db/migrate/20081205000001_portlet_names.rb
+++ db/migrate/20081205000001_portlet_names.rb
@@ -4,9 +4,10 @@ class PortletNames < ActiveRecord::Migration
     # and using the 'id' column for portletid does not work either,
     # ActiveRecord does not let that column to be set manually.
     create_table :portlet_names do |t|
-      t.column :portletid, :string, :null => false
-      t.column :name,      :string, :null => false
-      t.column :title,     :string, :null => false
+      t.column :portletid,    :string,  :null => false
+      t.column :name,         :string,  :null => false
+      t.column :title,        :string,  :null => false
+      t.column :instanceable, :boolean, :null => false, :default => true
     end
   end
 
