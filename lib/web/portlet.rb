33719a931279238bef1c107bcade752e09a1f2f9
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-02-25 09:26:11 +0000

    work on inserting portlets to layouts

--- lib/web/portlet.rb
+++ lib/web/portlet.rb
@@ -27,9 +27,47 @@ module Web
         |p| p.name=='%s' % name }.first # find_by_name
     end
 
+    # read-only
     def title
       p = Web::PortletName.find_by_portletid(self.portletid)
       p ? p.title : nil
+    end
+
+    # Is the portlet instanceable? This is defined in the XML configuration.
+    # TODO: parse to database.
+    def instanceable?
+      return false if self.portletid=='58' # login
+      true
+    end
+
+#     def self.find_by_portletid(*args)
+#     puts args.inspect
+#       super(args)
+#       # TODO: find caterpillar_portlets
+#     end
+
+    # searches both Liferay and Caterpillar portlets
+    def self.find_by_name(name)
+      begin
+        pn = Web::PortletName.find_by_name(name)
+        if pn
+          p = self.find_by_portletid pn.portletid
+          return p if p
+        end
+        
+        pn = find_caterpillar_portlet(name) unless pn
+        
+        unless pn
+          raise ActiveRecord::RecordNotFound
+        else
+          return self.create(
+            :portletid => pn.portletid
+          )
+        end
+      rescue
+        STDERR.puts 'portlet by name %s could not be found -- try "caterpillar db:migrate"' % name
+        raise $!
+      end
     end
 
   end
