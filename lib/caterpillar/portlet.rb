87569e4bc10b2b0b6734c4d79d0ab333d316cb20
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-03-06 09:32:03 +0000

    cleanup and commentary

--- lib/caterpillar/portlet.rb
+++ lib/caterpillar/portlet.rb
@@ -56,24 +56,33 @@ module Caterpillar
     # portlet.xml template.
     def template(portlet)
       xml =  "  <!-- %s -->\n" % portlet[:title]
+
       xml << "  <portlet>\n"
+      ### identification
       xml << "    <portlet-name>%s</portlet-name>\n" % portlet[:name]
       xml << "    <portlet-class>%s</portlet-class>\n" % self.portlet_class
+      ### supported portlet modes
       xml << "    <supports>\n"
       xml << "      <mime-type>text/html</mime-type>\n"
       xml << "      <portlet-mode>view</portlet-mode>\n"
-      ### edit mode
-      xml << "      <portlet-mode>edit</portlet-mode>\n" if portlet[:edit]==true
+      # edit mode is not used. this is for development.
+      if portlet[:edit]==true
+        xml << "      <portlet-mode>edit</portlet-mode>\n"
+      end
       xml << "    </supports>\n"
+      ### title for portlet container
       xml << "    <portlet-info>\n"
       xml << "      <title>%s</title>\n" % portlet[:title]
       xml << "    </portlet-info>\n"
-      xml << "  </portlet>\n"
-      xml << ""
+      xml << "  </portlet>\n\n"
+
+      ### portlet filters
       xml << "  <filter>\n"
+      # the filter reads the settings and sets the portlet session
       xml << "    <filter-name>%s_filter</filter-name>\n" % portlet[:name]
       xml << "    <filter-class>%s</filter-class>\n" % self.portlet_filter_class
       xml << "    <lifecycle>RENDER_PHASE</lifecycle>\n"
+      # define host, servlet and route (path to be more precise)
       xml << "    <init-param>\n"
       xml << "      <name>host</name>\n"
       xml << "      <value>%s</value>\n" % portlet[:host] || ""
@@ -86,8 +95,8 @@ module Caterpillar
       xml << "      <name>route</name>\n"
       xml << "      <value>%s</value>\n" % portlet[:path].gsub(/&/,"&amp;")
       xml << "    </init-param>\n"
-      xml << "  </filter>\n"
-      xml << ""
+      xml << "  </filter>\n\n"
+
       xml << "  <filter-mapping>\n"
       xml << "    <filter-name>%s_filter</filter-name>\n" % portlet[:name]
       xml << "    <portlet-name>%s</portlet-name>\n" % portlet[:name]
