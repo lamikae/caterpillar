1ee80cb5abe9b3aa27266209b68abc1ce24ac5ea
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-07-09 22:33:40 +0000

    cleanup

---
+++ test/liferay_helpers_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+require File.dirname(File.expand_path(__FILE__))+'/test_helper'
+
+class LiferayHelpersTest < Caterpillar::TestCase # :nodoc:
+  def test_link_to_liferay
+    user = User.first
+
+    flunk
+  end
+end
