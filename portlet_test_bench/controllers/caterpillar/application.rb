11314f8760db60aa15d9b69d3098b39781eda836
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-09-29 11:55:35 +0000

    added portlet test for flash messages

--- portlet_test_bench/controllers/caterpillar/application.rb
+++ portlet_test_bench/controllers/caterpillar/application.rb
@@ -7,8 +7,10 @@ class Caterpillar::ApplicationController < ActionController::Base
   before_filter :is_test_selected
 
   # Rails-portlet does not have session cookie support.
-  session :disabled => true
+  # But flash messages require this.
+  #session :disabled => true
 
+  # If controller is not ApplicationController, test is selected.
   def is_test_selected
     @test_is_selected = self.class.to_s[/Application/].nil?
   end
--- portlet_test_bench/controllers/caterpillar/session_controller.rb
+++ portlet_test_bench/controllers/caterpillar/session_controller.rb
@@ -1,57 +1,59 @@
 class Caterpillar::SessionController < Caterpillar::ApplicationController
 
-  def initialize
-
+  # flash message.
+  def flash_msg
+    flash[:info] = "Flash works."
+    redirect_to :action => :flash_display
   end
 
-  def index
-    render :inline => VERSION
-  end
-
-
-  # inspect the session variables
-  def raw
-    render :inline => session.inspect
-  end
-
-
-  # inspect the session cookie
-  def cookie
-
-    s = "<html><body><pre>"
-
-    cookies.each do |cookie|
-#       s += "name: " + cookie[0]
-      s += cookie.inspect
-      s += "<br />"
-#       s += "id: " + cookie[1].to_s
+#   def index
+#     render :inline => VERSION
+#   end
+# 
+# 
+#   # inspect the session variables
+#   def raw
+#     render :inline => session.inspect
+#   end
+# 
+# 
+#   # inspect the session cookie
+#   def cookie
+# 
+#     s = "<html><body><pre>"
+# 
+#     cookies.each do |cookie|
+# #       s += "name: " + cookie[0]
+#       s += cookie.inspect
 #       s += "<br />"
-# cookie.methods.each { |m| s += m.to_s + "<br />" }
-
-    end
-
-    s += "</pre></body></html>"
-    render :inline => s
-  end
-
-  def _id
-    render :inline => session.session_id.inspect
-  end
-
-
-  # inspect the request variables
-  def req
-    s = "<html><body>"
-
-    request.env.each_pair do |key,value|
-      s += key
-      s += " => "
-      s += value unless value.nil?
-      s += "<br />"
-    end
-
-    s += "</body></html>"
-    render :inline => s
-  end
+# #       s += "id: " + cookie[1].to_s
+# #       s += "<br />"
+# # cookie.methods.each { |m| s += m.to_s + "<br />" }
+# 
+#     end
+# 
+#     s += "</pre></body></html>"
+#     render :inline => s
+#   end
+# 
+#   def _id
+#     render :inline => session.session_id.inspect
+#   end
+# 
+# 
+#   # inspect the request variables
+#   def req
+#     s = "<html><body>"
+# 
+#     request.env.each_pair do |key,value|
+#       s += key
+#       s += " => "
+#       s += value unless value.nil?
+#       s += "<br />"
+#     end
+# 
+#     s += "</body></html>"
+#     render :inline => s
+#   end
 
 end
