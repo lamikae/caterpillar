d1ce4762c78bf127a06209284564481eee30310b
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2009-11-28 13:11:49 +0000

    simple cookie test

--- portlet_test_bench/controllers/caterpillar/session_controller.rb
+++ portlet_test_bench/controllers/caterpillar/session_controller.rb
@@ -17,25 +17,35 @@ class Caterpillar::SessionController < Caterpillar::ApplicationController
 #   end
 # 
 # 
-#   # inspect the session cookie
-#   def cookie
-# 
-#     s = "<html><body><pre>"
-# 
-#     cookies.each do |cookie|
-# #       s += "name: " + cookie[0]
-#       s += cookie.inspect
+  # inspect the session cookie
+
+	def cookie_with_redirect
+		cookies[:the_time] = Time.now.to_s
+		redirect_to :action => "cookie_with_redirect_target"
+	end
+	def cookie_with_redirect_target
+		cookie_value = cookies[:the_time]
+		render(:text => "The cookie says it is #{cookie_value}")
+	end
+
+	def cookie
+		cookies[:platform] = RUBY_PLATFORM
+		cookies[:the_time] = Time.now.to_s
+
+
+    cookies.each do |cookie|
+#       s += "name: " + cookie[0]
+      logger.debug cookie.inspect
+#       s += "id: " + cookie[1].to_s
 #       s += "<br />"
-# #       s += "id: " + cookie[1].to_s
-# #       s += "<br />"
-# # cookie.methods.each { |m| s += m.to_s + "<br />" }
-# 
-#     end
-# 
-#     s += "</pre></body></html>"
-#     render :inline => s
-#   end
-# 
+# cookie.methods.each { |m| s += m.to_s + "<br />" }
+
+    end
+
+	session[:testvalue] = "Session data set at " + Time.now.to_s
+
+  end
+
 #   def _id
 #     render :inline => session.session_id.inspect
 #   end
