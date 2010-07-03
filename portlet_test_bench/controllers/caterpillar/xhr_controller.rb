8306d0458b1e37b08c29a3fc5387f2e54159adab
lamikae <mikael.lammentausta@gmail.com>
2010-07-03 16:28:28 +0300

    prototype and jQuery XHR demos

--- portlet_test_bench/controllers/caterpillar/xhr_controller.rb
+++ portlet_test_bench/controllers/caterpillar/xhr_controller.rb
@@ -1,6 +1,20 @@
 # encoding: utf-8
 class Caterpillar::XhrController < Caterpillar::ApplicationController
 
-  def initialize
+  def prototype
+    # the page with the XHR triggers
+    @javascripts = %w{prototype}
+    render :action => 'time'
   end
+
+  def jquery
+    # the page with the XHR triggers
+    @javascripts = %w{jquery-1.4.2.min}
+    render :action => 'time'
+  end
+
+  def get_time
+    send_data Time.now.to_s, :type => 'text/html'
+  end
+
 end
