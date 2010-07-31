6ed07c1eb640023b5cbc410ae77b0632be202394
lamikae <mikael.lammentausta@gmail.com>
2010-07-31 15:01:23 +0300

    relocate test

--- test/liferay_helpers_test.rb
+++ test/liferay_helpers_test.rb
@@ -13,6 +13,24 @@ class LiferayHelpersTest < ActionController::TestCase # :nodoc:
     assert true
   end
   
+  def test_get_liferay_preferences
+    # Simulated value from the cookie sent
+    value = "font_size=20px;background_color=#000000;page_size=20;"
+    
+    hash = get_liferay_preferences(value)
+    assert_not_nil hash
+    assert_equal 3, hash.length
+    [:font_size, :background_color, :page_size].each do |key|
+      hash.has_key? key
+    end
+    
+    ["20px", "#000000", "20"].each do |value|
+      hash.has_value? value
+    end
+    
+    assert_nil get_liferay_preferences(nil)
+  end
+  
   def test_liferay_resource_url
     # Simulated value from the cookie sent
     resource_url = "http://localhost:8080/resourceUrl"
--- test/portlet_support_test.rb
+++ test/portlet_support_test.rb
@@ -4,22 +4,4 @@ require File.dirname(File.expand_path(__FILE__))+'/test_helper'
 class PortletSupportTest < ActionController::TestCase # :nodoc:
   include Caterpillar::PortletSupport
   
-  def test_get_liferay_preferences
-    # Simulated value from the cookie sent
-    value = "font_size=20px;background_color=#000000;page_size=20;"
-    
-    hash = get_liferay_preferences(value)
-    assert_not_nil hash
-    assert_equal 3, hash.length
-    [:font_size, :background_color, :page_size].each do |key|
-      hash.has_key? key
-    end
-    
-    ["20px", "#000000", "20"].each do |value|
-      hash.has_value? value
-    end
-    
-    assert_nil get_liferay_preferences(nil)
-  end
-  
 end
