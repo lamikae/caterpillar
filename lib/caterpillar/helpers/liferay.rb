368a645e5f1f2050f504b2320b9ed4e35f27f7d1
lamikae <mikael.lammentausta@gmail.com>
2010-07-31 14:39:24 +0300

    refactor get_liferay_preferences to Helpers::Liferay

--- lib/caterpillar/helpers/liferay.rb
+++ lib/caterpillar/helpers/liferay.rb
@@ -51,6 +51,22 @@ module Helpers # :nodoc:
       url
     end
 
+    # Gets portlet preferences from a cookie (Liferay_preferences) and generates
+    # a hash with it. Returns nil if cookie do not exists or the value is nil.
+    #
+    def get_liferay_preferences(value = cookies[:Liferay_preferences])
+      preferences = {}
+      if value and (not value.empty?)
+        value.split(";").each do |pair|
+          if pair.nil? or pair.empty? then next end
+          result = pair.split("=")
+          preferences[result[0].intern] = result[1]
+        end
+        return preferences
+      end
+      nil
+    end
+
     # Link that the rails-portlet will leave unparsed.
     def link_to_exit_portlet(name, options = {}, html_options = {})
       link_to(name, url_to_exit_portlet(options), html_options)
--- lib/caterpillar/portlet_support.rb
+++ lib/caterpillar/portlet_support.rb
@@ -8,26 +8,9 @@
 
 module Caterpillar
   
-  # Add some portlet support
+  # Add some portlet support (DEPRECATED)
   #
   module PortletSupport
-  
-    # Gets portlet preferences from a cookie (Liferay_preferences) and generates
-    # a hash with it. Returns nil if cookie do not exists or the value is nil.
-    # XXX: should not this be moved to Helpers::Liferay ?
-    #
-    def get_liferay_preferences(value = cookies[:Liferay_preferences])
-      preferences = {}
-      if value and (not value.empty?)
-        value.split(";").each do |pair|
-          if pair.nil? or pair.empty? then next end
-          result = pair.split("=")
-          preferences[result[0].intern] = result[1]
-        end
-        return preferences
-      end
-      nil
-    end
-
+    include Caterpillar::Helpers::Liferay
   end
 end
