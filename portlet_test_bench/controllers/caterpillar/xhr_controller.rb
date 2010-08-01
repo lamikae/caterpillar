435a7efed918a0674fe4de93f54ca1971c058256
lamikae <mikael.lammentausta@gmail.com>
2010-08-01 03:43:15 +0300

    view with resource url

--- portlet_test_bench/controllers/caterpillar/xhr_controller.rb
+++ portlet_test_bench/controllers/caterpillar/xhr_controller.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class Caterpillar::XhrController < Caterpillar::ApplicationController
-
+  include Caterpillar::Helpers::Liferay
+  
   def prototype
     # the page with the XHR triggers
     @javascripts = %w{prototype}
@@ -15,6 +16,16 @@ class Caterpillar::XhrController < Caterpillar::ApplicationController
 
   def get_time
     send_data Time.now.to_s, :type => 'text/html'
+  end
+
+  # Liferay resource URL
+  def resource
+    logger.debug params
+    @resource_url = cookies[:Liferay_resourceUrl]
+    logger.debug @resource_url
+    @r_u_params = params.dup
+    #p url_for(:controller => Caterpillar::XhrController )
+    
   end
 
 end
